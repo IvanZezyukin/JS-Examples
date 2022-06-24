9966e42d8333ec6aa90278ab7c3f618c510458a1
IvanZezyukin <i.zezukin@gmail.com>
2022-06-24 16:12:53 +0300

    add example

--- Examples.js
+++ Examples.js
@@ -1885,3 +1885,21 @@ const cloneDeep = (object) => {
     return clonedObject;
 }
  */
+
+/*
+// Обход и возврат свойств и значений объекта
+const users = [
+    { name: 'Bronn', gender: 'male', birthday: '1973-03-23' },
+    { name: 'Reigar', gender: 'male', birthday: '1973-11-03' },
+    { name: 'Eiegon', gender: 'male', birthday: '1963-11-03' },
+    { name: 'Sansa', gender: 'female', birthday: '2012-11-03' },
+];
+const getSortedNames = (users) => {
+    let names = [];
+    for (const {name, ...rest} of users) {
+        names.push(name);
+    }
+    return names.sort();
+};
+console.log(getSortedNames(users));
+*/
