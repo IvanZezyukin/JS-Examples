851d12d476fcd569eb7f313a4798fff04b552f48
IvanZezyukin <i.zezukin@gmail.com>
2022-06-27 20:42:57 +0300

    example

--- Examples.js
+++ Examples.js
@@ -1927,3 +1927,28 @@ const sayPrimeOrNot = (num) => {
 
 export default sayPrimeOrNot;
  */
+
+/*
+// редьюс фильтрация вывод сортировка по ключу
+const students = [
+    { name: 'Tirion', class: 'B', mark: 2 },
+    { name: 'Keit', class: 'A', mark: 3 },
+    { name: 'Ramsey', class: 'A', mark: 4 },
+    { name: 'Bronn', class: 'B', mark: 3 },
+    { name: 'Sam', class: 'A', mark: 2 },
+    { name: 'Aria', class: 'B', mark: 5 },
+    { name: 'Keit', class: 'B', mark: 4 },
+    { name: 'Rob', class: 'B', mark: 4 },
+    { name: 'Taywin', class: 'A', mark: 5 },
+];
+const groupBy = (arr, key) => {
+    return arr.reduce((acc, item) => {
+        if (!Object.hasOwnProperty.bind(acc)(item[key])) {
+            acc[item[key]] = [];
+        }
+        acc[item[key]].push(item);
+        return acc;
+    }, {});
+};
+console.log(groupBy(students, 'class'));
+ */
