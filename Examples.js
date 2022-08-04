47310c19e46e970ce81f2820185e9c9adf43a949
Ivan Zezyukin <i.zezukin@gmail.com>
2022-08-04 22:30:35 +0300

    yup

--- Examples.js
+++ Examples.js
@@ -2185,3 +2185,26 @@ const magic = (...numbers) => {
   ];
   console.log(normalize(countries));
   */
+
+  /*
+  // yup валидатор builder сборщик 
+  import yup from 'yup';
+  const genres = [
+    'drama', 'horror', 'fantasy', 'classic',
+  ];
+  const getInvalidBooks = (books) => {
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      author: yup.string().required(),
+      pagesCount: yup.number().positive().integer(),
+      link: yup.string().url().min(1),
+      genre: yup.string().oneOf(genres)
+    });
+    return books.filter((book) => !schema.isValidSync(book));
+  }
+  const books = [
+    { name: 'book', author: 'author' },
+    { author: 'author 2' },
+  ];
+  console.log(getInvalidBooks(books));
+*/
