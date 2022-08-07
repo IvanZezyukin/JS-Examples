4690b67f78b8dc1fd57b173692288e0e6d247b12
Ivan Zezyukin <i.zezukin@gmail.com>
2022-08-07 16:11:40 +0300

    yup

--- Examples.js
+++ Examples.js
@@ -2208,3 +2208,29 @@ const magic = (...numbers) => {
   ];
   console.log(getInvalidBooks(books));
 */
+
+/*
+// отслеживание изменения файла
+export default (filepath, period, cb) => {
+    let lastCheckTime = Date.now();
+  
+    const check = (timerId) => {
+      fs.stat(filepath, (err, stats) => {
+        if (err) {
+          clearInterval(timerId);
+          cb(err);
+          return;
+        }
+        const { mtimeMs } = stats;
+        if (mtimeMs > lastCheckTime) {
+          cb(null);
+          lastCheckTime = mtimeMs;
+        }
+      });
+    };
+  
+    const timerId = setInterval(() => check(timerId), period);
+    return timerId;
+  };
+  */
+ 
